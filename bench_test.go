9af6e2c62d3381a0db9cbbdb270146bca4f9ee6e
David Bacisin <david@davidbacisin.com>
2025-04-21 21:22:58 -0700

    benchmark

---
+++ bench_test.go
@@ -0,0 +1,21 @@
+package mwnd_test
+
+import (
+	"math/rand/v2"
+	"testing"
+
+	"github.com/davidbacisin/go-mwnd"
+)
+
+func BenchmarkWindow_1000(b *testing.B) {
+	w := mwnd.New[int](1000)
+	for range b.N {
+		v := rand.Int()
+		w.Put(v)
+
+		if w.Min() < 0 || w.Max() < 0 || w.Mean() < 0.0 {
+			b.Logf("invalid min, max, or mean")
+			b.FailNow()
+		}
+	}
+}
--- doc.go
+++ doc.go
@@ -1,4 +1,10 @@
-// Package mwnd provides an on-line moving window implementation for computing
-// the minimum, maximum, mean, and total sum of squared differences from
+// Package mwnd provides a high-performance on-line moving window implementation for
+// computing the minimum, maximum, mean, and total sum of squared differences from
 // the mean over a stream of numeric values.
+//
+// The worst-case time complexity of adding a new sample to the window is O(log n),
+// where n is the capacity of the window. This is achieved by maintaining a red-black
+// balanced binary tree as the underlying data structure. Furthermore, the Put and
+// all statistical operations avoid memory allocations to preserve performance in
+// high-scale environments.
 package mwnd
